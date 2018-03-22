c7d3531d20273fcbfa86d395b0486698884f2121
Tom <demto13@gmail.com>
2018-03-22 20:59:08 +0000

    Initial commit

--- EventHandlerPractice/Program.cs
+++ EventHandlerPractice/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace EventHandlerPractice {
     class Program {
         static void Main(string[] args) {
+
+            var encoder = new VideoEncoder();
+            var vid = new Video { Title = "Greatest Moie" };
+
+            encoder.Encode(vid);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ EventHandlerPractice/VideoEncoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandlerPractice {
+    class VideoEncoder {
+
+        public event EventHandler<EventArgs> VideoEncoded;
+
+        public void Encode(Video video){
+            Console.WriteLine("Video is encoded!");
+        }
+    }
+}
